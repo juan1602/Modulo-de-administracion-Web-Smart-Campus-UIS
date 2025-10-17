17bd6986922dc7e17f6df16e9a1206ea6c35966a
Jean Carlo Rodriguez Pico <jean2214116@correo.uis.edu.co>
2025-10-17 11:26:48 -0500

    prueba aplicacion controller

--- backend-AdminSmartCampusUIS/src/main/java/com/uis/AdminSmartCampusUIS/backend_AdminSmartCampusUIS/controller/ApplicationController.java
+++ backend-AdminSmartCampusUIS/src/main/java/com/uis/AdminSmartCampusUIS/backend_AdminSmartCampusUIS/controller/ApplicationController.java
@@ -2,7 +2,7 @@ package com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.controller;
 
 import java.util.List;
 
-import jakarta.*;
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,8 +19,9 @@ import com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.dto.ApplicationDe
 import com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.dto.ApplicationForm;
 import com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.dto.ApplicationServiceForm;
 import com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.dto.DeviceApplicationForm;
-import com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS.service.ApplicationService;
+import com.iot.admin.admin.service.ApplicationService;
 
+//pruba modulo aplicacion
 @RestController
 @RequestMapping("apps")
 public class ApplicationController {
@@ -29,46 +30,44 @@ public class ApplicationController {
     private ApplicationService service;
 
     @GetMapping
-    public List<ApplicationDetail> list(){       
+    public List<ApplicationDetail> list() {
         return service.getAll();
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ApplicationDetail create(@RequestBody @Valid ApplicationForm data){        
+    public ApplicationDetail create(@RequestBody @Valid ApplicationForm data) {
         return service.create(data);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public boolean delete(@PathVariable Long id){        
+    public boolean delete(@PathVariable Long id) {
         return service.delete(id);
     }
 
     @PostMapping("/device/{app_id}")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public boolean createDevice(@RequestBody @Valid DeviceApplicationForm data,@PathVariable Long app_id){        
-        return service.addDevice(data,app_id);
+    public boolean createDevice(@RequestBody @Valid DeviceApplicationForm data, @PathVariable Long app_id) {
+        return service.addDevice(data, app_id);
     }
 
     @DeleteMapping("/device/{app_id}")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public boolean deleteDevice(@RequestBody @Valid DeviceApplicationForm data,@PathVariable Long app_id){        
-        return service.deleteDevice(data,app_id);
+    public boolean deleteDevice(@RequestBody @Valid DeviceApplicationForm data, @PathVariable Long app_id) {
+        return service.deleteDevice(data, app_id);
     }
 
     @PostMapping("/service/{app_id}")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public boolean addService(@RequestBody @Valid ApplicationServiceForm data,@PathVariable Long app_id){        
-        return service.addService(data,app_id);
+    public boolean addService(@RequestBody @Valid ApplicationServiceForm data, @PathVariable Long app_id) {
+        return service.addService(data, app_id);
     }
 
     @DeleteMapping("/service/{app_id}")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public boolean removeService(@RequestBody @Valid ApplicationServiceForm data,@PathVariable Long app_id){        
-        return service.deleteService(data,app_id);
+    public boolean removeService(@RequestBody @Valid ApplicationServiceForm data, @PathVariable Long app_id) {
+        return service.deleteService(data, app_id);
     }
 
-
-    
 }
