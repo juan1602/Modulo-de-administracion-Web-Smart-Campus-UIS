bdc3f366ee046d01d744f7da999bc29782715fbd
Juan Pablo Avila Quitian <juan2214107@correo.uis.edu.co>
2025-10-17 08:56:07 -0500

    primer commit

---
+++ backend-AdminSmartCampusUIS/src/main/java/com/uis/AdminSmartCampusUIS/backend_AdminSmartCampusUIS/BackendAdminSmartCampusUisApplication.java
@@ -0,0 +1,13 @@
+package com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminSmartCampusUisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdminSmartCampusUisApplication.class, args);
+	}
+
+}
---
+++ backend-AdminSmartCampusUIS/src/test/java/com/uis/AdminSmartCampusUIS/backend_AdminSmartCampusUIS/BackendAdminSmartCampusUisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uis.AdminSmartCampusUIS.backend_AdminSmartCampusUIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdminSmartCampusUisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
